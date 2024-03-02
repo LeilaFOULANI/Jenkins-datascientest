ae344cb4ad183fb6c5914473a717af92a2897d0a
LeilaFOULANI <161530246+LeilaFOULANI@users.noreply.github.com>
2024-03-02 22:47:19 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
-from typing import Union
 from fastapi import FastAPI
-app = FastAPI()
-@app.get("/")
-def read_root():
-    return {"Hello": "We Love Datascientest !!"}
+from app.api.movies import movies
+from app.api.db import metadata, database, engine
+
+metadata.create_all(engine)
+
+app = FastAPI(openapi_url="/api/v1/movies/openapi.json", docs_url="/api/v1/movies/docs")
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+
+app.include_router(movies, prefix='/api/v1/movies', tags=['movies'])
